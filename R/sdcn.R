1b62a1b8c2b8fac1d84d89cf133dabce4a529884
fengwenfeng <feng@fengwenfengdeMacBook-Air.local>
2015-02-10 13:48:39 +0000

    Description of Package

---
+++ R/sdcn.R
@@ -0,0 +1,11 @@
+#' sdcn: Structures and Dynamics on (of) Complex Networks.
+#'
+#' The sdcn package provides three categories of important functions:
+#' \code{\link{Summary}}.
+#' 
+#' @section Sdcn functions:
+#' \code{\link{Summary}}
+#'
+#' @docType package
+#' @name sdcn
+NULL
